5dbef191899da0567157eb834d33ae171f9bc640
Greg Turnquist <gturnquist@pivotal.io>
2016-04-28 22:43:34 -0500

    Initial commit for Learning Spring Boot video

---
+++ src/test/java/com/greglturnquist/LearningSpringBootVideoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greglturnquist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearningSpringBootVideoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
