f0684ec0c620d2a2a7c5fafbc59d1307e12ae8d5
Greg Turnquist <gturnquist@pivotal.io>
2016-05-29 21:03:23 -0500

    Capture end of volume 1's code

---
+++ volume-1/src/main/java/com/greglturnquist/learningspringboot/LearningSpringBootVideoApplication.java
@@ -0,0 +1,12 @@
+package com.greglturnquist.learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootVideoApplication.class, args);
+	}
+}
